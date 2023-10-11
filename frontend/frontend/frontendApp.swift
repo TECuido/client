b35bcd8216fd49702e5744a6417de2ac3c0cb74b
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2023-10-11 15:56:51 -0600

    Inicio de swift

---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by Alumno on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
