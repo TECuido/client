c185a6bcc2884639de16ab396877a8208805f41e
Bernardo de la Sierra Rábago <93608793+Bdelas777@users.noreply.github.com>
2023-10-13 17:54:16 -0600

    Emergencias

--- TECuido/SinEmergenciasView.swift
+++ TECuido/SinEmergenciasView.swift
@@ -11,21 +11,20 @@ struct SinEmergenciasView: View {
     var body: some View {
         ZStack{
             VStack{
-                
+                // icono de la pantalla
                 Image("icon")
                     .resizable()
                     .frame(width: 60, height: 60)
                     .position(x:60,y:20)
                 Spacer()
-                
-            
-        
+                // Titulo
                 Text("Emergencias")
                     .foregroundColor(Color(red: 0.1294,green: 0.5882,blue: 0.9529))
                     .font(.system(size: 55))
                     .bold()
-                
+                // Imagen principal
                 Image("bien")
+                // Texto de todo bien
                 Text("Todo se")
                     .font(.largeTitle)
                     .bold()
@@ -36,6 +35,7 @@ struct SinEmergenciasView: View {
                     .font(.largeTitle)
                     .bold()
                 //Boton
+                // Aqui falta usar la navegacion
                 Button("Enviar Emergencia"){
                     
                 }
