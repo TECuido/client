ac2bf46bb56cee796418726984873221c2452496
Mariano Barberi Garza <93608793+Bdelas777@users.noreply.github.com>
2024-01-13 16:02:35 -0600

    Deletes

--- TECuido/Model/Perfil/AlergiaModel.swift
+++ TECuido/Model/Perfil/AlergiaModel.swift
@@ -4,24 +4,42 @@
 //
 //  Created by Alumno on 09/01/24.
 //
-
 import Foundation
-
- struct AlergiaModel: Codable, Hashable {
+ struct AgregaAlergiaModel: Codable, Hashable {
      var nombre: String
      var idUsuario: Int
      enum CodingKeys: String, CodingKey {
          case nombre = "nombre"
          case idUsuario = "idUsuario"
      }
-
-
      func hash(into hasher: inout Hasher) {
          hasher.combine(nombre)
          hasher.combine(idUsuario)
      }
  }
-
-extension AlergiaModel {
-    static var example = AlergiaModel(nombre: "Polvo", idUsuario: 1)
+extension AgregaAlergiaModel {
+    static var example = AgregaAlergiaModel(nombre: "Polvo", idUsuario: 1)
 }
+
+struct AlergiaModel : Identifiable, Decodable, Hashable{
+    var id: Int
+    var nombre : String
+    var idUsuario : Int
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "idAlergia"
+        case nombre = "nombre"
+        case idUsuario
+    }
+    
+    func hash(into hasher: inout Hasher) {
+         hasher.combine(id)
+         hasher.combine(nombre)
+         hasher.combine(idUsuario)
+     }
+}
+extension AlergiaModel {
+    public static var example =
+    AlergiaModel(id: 1, nombre: "Polvo", idUsuario: 2)
+}
+
